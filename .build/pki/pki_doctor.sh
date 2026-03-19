5b7ee958f6267fefab395907aad354e36138a559
Masi-s-Thorue <etu51177@henallux.be>
2026-03-20 00:17:00 +0100

    Update Keycloak entrypoint for improved certificate handling and adjust PKI deployment scripts for better path references

--- .build/pki/pki_doctor.sh
+++ .build/pki/pki_doctor.sh
@@ -1,12 +1,12 @@
-openssl req -new -config identity.conf \
+openssl req -new -config /etc/pki/CA/config/identity.conf \
     -out docteur_martin.csr \
     -keyout docteur_martin.key \
-    -nodes # On génère une clé sans mot de passe pour simplifier l'export PKCS12 après
+    -nodes
 
-    # On définit l'autorité de signature
+# On définit l'autorité qui signe (Signing CA 1)
 export ca="signing-ca1"
 
-openssl ca -config signing-ca.conf \
+openssl ca -config /etc/pki/CA/config/signing-ca.conf \
     -in docteur_martin.csr \
     -out docteur_martin.crt \
     -extensions email_ext \
@@ -17,6 +17,8 @@ openssl pkcs12 -export \
     -in docteur_martin.crt \
     -inkey docteur_martin.key \
     -certfile /etc/pki/CA/certs/ca-chain.pem \
-    -name "Certificat Identité - Dr Martin" \
+    -name "Certificat Docteur Martin" \
     -out docteur_martin.p12 \
-    -passout pass:password_docteur # Mot de passe que le docteur devra taper pour l'importer
+    -passout pass:password_docteur
+
+cp docteur_martin.p12 /etc/pki/CA/config
