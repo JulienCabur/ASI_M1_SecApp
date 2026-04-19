4525f00aeaac8482b5238e597430663e91ea9ba9
Erdem2209 <erdemsunay.demir@gmail.com>
2026-04-19 17:46:59 +0200

    feat: enhance env file creation scripts; add overwrite prompt and fix variable names

--- .build/env-creation.sh
+++ .build/env-creation.sh
@@ -5,11 +5,22 @@ DEFAULT_PATH=$(dirname "$(realpath "$0")")
 echo "Creating .env files..."
 
 # .build/.env
-if [ ! -f "$DEFAULT_PATH/.env" ]; then
-    cp "$DEFAULT_PATH/env.example" "$DEFAULT_PATH/.env"
-    echo "Created $DEFAULT_PATH/.env"
-else
-    echo "$DEFAULT_PATH/.env already exists"
+if [ -f "$DEFAULT_PATH/.env" ]; then
+    echo "$DEFAULT_PATH/.env already exists. Do you want to overwrite it? (y/n)"
+    read response
+    if [ "$response" = "y" ]; then
+        cp "$DEFAULT_PATH/env.example" "$DEFAULT_PATH/.env"
+        echo "Overwritten $DEFAULT_PATH/.env"
+        exit 0
+    else
+        echo "Aborting. $DEFAULT_PATH/.env was not overwritten."
+        exit 0
+    fi
 fi
 
+cp "$DEFAULT_PATH/env.example" "$DEFAULT_PATH/.env"
+echo "Created $DEFAULT_PATH/.env"
+
 echo "Done!"
+
+exit 0
