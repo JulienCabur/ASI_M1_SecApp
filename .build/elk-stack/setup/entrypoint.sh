9d2d193dbcdb11f0a86728e47c7a9e1eb88657cf
Ciaran Mombaerts <etu51729@henallux.be>
2026-05-12 11:13:19 +0200

    feat: enhance ELK stack configuration with audit logging and security alerts

--- .build/elk-stack/setup/entrypoint.sh
+++ .build/elk-stack/setup/entrypoint.sh
@@ -12,6 +12,7 @@ source "${BASH_SOURCE[0]%/*}"/lib.sh
 declare -A users_passwords
 users_passwords=(
 	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
+	[logstash_alerts_writer]="${LOGSTASH_ALERTS_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
 	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
 	[filebeat_internal]="${FILEBEAT_INTERNAL_PASSWORD:-}"
@@ -23,6 +24,7 @@ users_passwords=(
 declare -A users_roles
 users_roles=(
 	[logstash_internal]='logstash_writer'
+	[logstash_alerts_writer]='security_alerts_writer'
 	[metricbeat_internal]='metricbeat_writer'
 	[filebeat_internal]='filebeat_writer'
 	[heartbeat_internal]='heartbeat_writer'
@@ -35,6 +37,8 @@ users_roles=(
 declare -A roles_files
 roles_files=(
 	[logstash_writer]='logstash_writer.json'
+	[security_alerts_writer]='security_alerts_writer.json'
+	[security_alerts_reader]='security_alerts_reader.json'
 	[metricbeat_writer]='metricbeat_writer.json'
 	[filebeat_writer]='filebeat_writer.json'
 	[heartbeat_writer]='heartbeat_writer.json'
